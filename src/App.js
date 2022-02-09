683581cb97aaf61c3044aaed236fadcac6f6fb49
An Quach <quachan888@gmail.com>
2022-02-09 13:52:11 -0500

    Redux: Product Detail Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProductDetails from './components/Product/ProductDetails';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Header />
                 <div className="container container-fluid">
                     <Route path="/" component={Home} exact />
+                    <Route path="/product/:id" exact component={ProductDetails} />
                 </div>
 
                 <Footer />
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,14 @@ import MetaData from './layout/MetaData';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../actions/productActions';
+import Product from './Product/Product';
+import Loader from './layout/Loader';
 
 const Home = () => {
     const dispatch = useDispatch();
     const { loading, products, error, productsCount } = useSelector((state) => state.products);
 
-    console.log(loading, products, error, productsCount);
+    // console.log(loading, products, error, productsCount);
 
     useEffect(() => {
         dispatch(getProducts());
@@ -17,37 +19,21 @@ const Home = () => {
     return (
         <>
             <MetaData title={'Buy best products online'} />
-            <h1 id="products_heading">Latest Products</h1>
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                    <h1 id="products_heading">Latest Products</h1>
 
-            <section id="products" className="container mt-5">
-                <div className="row">
-                    {products.map((product) => (
-                        <div className="col-sm-12 col-md-6 col-lg-3 my-3" key={product.id}>
-                            <div className="card p-3 rounded">
-                                <img className="card-img-top mx-auto" src={product.image} />
-                                <div className="card-body d-flex flex-column">
-                                    <h5 className="card-title">
-                                        <a href="">{product.title}</a>
-                                    </h5>
-                                    <div className="ratings mt-auto">
-                                        <div className="rating-outer">
-                                            <div
-                                                className="rating-inner"
-                                                style={{ width: `${(product.rating.rate / 5) * 100}%` }}
-                                            ></div>
-                                        </div>
-                                        <span id="no_of_reviews">({product.rating.count} Reviews)</span>
-                                    </div>
-                                    <p className="card-text">${product.price}</p>
-                                    <a href="#" id="view_btn" className="btn btn-block">
-                                        View Details
-                                    </a>
-                                </div>
-                            </div>
+                    <section id="products" className="container mt-5">
+                        <div className="row">
+                            {products.map((product) => (
+                                <Product key={product.id} product={product} />
+                            ))}
                         </div>
-                    ))}
-                </div>
-            </section>
+                    </section>
+                </>
+            )}
         </>
     );
 };
